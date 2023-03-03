6a46e3f377949d139fe179acbe3c742e61c37ad8
Hoang Dang <bdhoang.ht@gmail.com>
2023-03-03 12:27:56 +0700

    add telegram bot

--- function/send-telegram.js
+++ function/send-telegram.js
@@ -1,1 +1,21 @@
-module.exports = {}
+const axios = require("axios");
+
+async function sendTelegram(data, config, callback) {
+  const bot_token = config.telegram.bot_token;
+  const chat_id = config.telegram.chat_id;
+  const content = data.text;
+  axios
+    .get(
+      `https://api.telegram.org/bot${bot_token}/sendMessage?chat_id=${chat_id}&text=${content}`
+    )
+    .then((res) => {
+      console.log(res.data);
+      callback(null, res.data);
+    })
+    .catch((err) => {
+      console.log(err);
+      callback(err, null);
+    });
+}
+
+module.exports = { sendTelegram };
--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const sendMail = require('./function/send-mail').sendMail;
+const sendTelegram = require('./function/send-telegram').sendTelegram;
 
 class wiNotification {
     constructor(config) {
         this.config = config
     }
     sendMail(options) {
+        if (!this.config?.user) return console.log("No smtp user");
+        if (!this.config?.password) return console.log("No smtp password");
         if (!options.to) return console.log("No destination email");
         if (!options.messageHtml) return console.log("No message");
         if (!options.subject) return console.log("No subject");
@@ -27,7 +30,23 @@ class wiNotification {
         })
     }
     sendTelegramMessage(options) {
-        return;
+        if (!this.config?.telegram?.bot_token) return console.log("No bot token");
+        if (!this.config?.telegram?.chat_id) return console.log("No chat id");
+        if (!options.text) return console.log("No text");
+        sendTelegram({
+            text: options.text
+        }, {
+            telegram: {
+                bot_token: this.config.telegram.bot_token,
+                chat_id: this.config.telegram.chat_id
+            }
+        }, function (err, resp) {
+            if (err) {
+                console.log("Send notification telegram error ", err);
+            } else {
+                console.log("Send notification telegram done ", resp);
+            }
+        })
     }
 
     sendSlackMessage(options) {
